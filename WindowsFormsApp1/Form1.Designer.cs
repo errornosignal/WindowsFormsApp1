1a4da4a40a5e338f3f86a7f31324c773cf3a6918
Reid Nolan <errornosignal@gmail.com>
2018-01-31 11:34:53 -0600

    InClassCode_1_31

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -34,12 +34,24 @@
             this.WidthTextBox = new System.Windows.Forms.TextBox();
             this.CalculateButton = new System.Windows.Forms.Button();
             this.ResultsTextBox = new System.Windows.Forms.TextBox();
+            this.ShippingGroupBox = new System.Windows.Forms.GroupBox();
+            this.StandardShippingRadioButton = new System.Windows.Forms.RadioButton();
+            this.ExpeditedShippingRadioButton = new System.Windows.Forms.RadioButton();
+            this.InternationalShippingRadioButton = new System.Windows.Forms.RadioButton();
+            this.ShippingChargesLabel = new System.Windows.Forms.Label();
+            this.ShippingChargesTextBox = new System.Windows.Forms.TextBox();
+            this.CalculateShippingChargesButton = new System.Windows.Forms.Button();
+            this.ShippingWeightTextBox = new System.Windows.Forms.TextBox();
+            this.ShippingWeightLabel = new System.Windows.Forms.Label();
+            this.ResetFormButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.ShippingGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // LengthLabel
             // 
             this.LengthLabel.AutoSize = true;
-            this.LengthLabel.Location = new System.Drawing.Point(12, 9);
+            this.LengthLabel.Location = new System.Drawing.Point(47, 9);
             this.LengthLabel.Name = "LengthLabel";
             this.LengthLabel.Size = new System.Drawing.Size(40, 13);
             this.LengthLabel.TabIndex = 0;
@@ -48,7 +60,7 @@
             // WidthLabel
             // 
             this.WidthLabel.AutoSize = true;
-            this.WidthLabel.Location = new System.Drawing.Point(17, 53);
+            this.WidthLabel.Location = new System.Drawing.Point(52, 31);
             this.WidthLabel.Name = "WidthLabel";
             this.WidthLabel.Size = new System.Drawing.Size(35, 13);
             this.WidthLabel.TabIndex = 1;
@@ -56,24 +68,26 @@
             // 
             // LengthTextBox
             // 
-            this.LengthTextBox.Location = new System.Drawing.Point(84, 6);
+            this.LengthTextBox.AcceptsReturn = true;
+            this.LengthTextBox.Location = new System.Drawing.Point(93, 6);
             this.LengthTextBox.Name = "LengthTextBox";
             this.LengthTextBox.Size = new System.Drawing.Size(100, 20);
-            this.LengthTextBox.TabIndex = 2;
+            this.LengthTextBox.TabIndex = 0;
             // 
             // WidthTextBox
             // 
-            this.WidthTextBox.Location = new System.Drawing.Point(84, 50);
+            this.WidthTextBox.AcceptsReturn = true;
+            this.WidthTextBox.Location = new System.Drawing.Point(93, 28);
             this.WidthTextBox.Name = "WidthTextBox";
             this.WidthTextBox.Size = new System.Drawing.Size(100, 20);
-            this.WidthTextBox.TabIndex = 3;
+            this.WidthTextBox.TabIndex = 1;
             // 
             // CalculateButton
             // 
-            this.CalculateButton.Location = new System.Drawing.Point(15, 99);
+            this.CalculateButton.Location = new System.Drawing.Point(12, 57);
             this.CalculateButton.Name = "CalculateButton";
             this.CalculateButton.Size = new System.Drawing.Size(75, 23);
-            this.CalculateButton.TabIndex = 4;
+            this.CalculateButton.TabIndex = 2;
             this.CalculateButton.Text = "Calculate";
             this.CalculateButton.UseVisualStyleBackColor = true;
             this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
@@ -81,16 +95,136 @@
             // ResultsTextBox
             // 
             this.ResultsTextBox.Enabled = false;
-            this.ResultsTextBox.Location = new System.Drawing.Point(106, 101);
+            this.ResultsTextBox.Location = new System.Drawing.Point(93, 60);
             this.ResultsTextBox.Name = "ResultsTextBox";
             this.ResultsTextBox.Size = new System.Drawing.Size(100, 20);
             this.ResultsTextBox.TabIndex = 5;
+            // 
+            // ShippingGroupBox
+            // 
+            this.ShippingGroupBox.Controls.Add(this.InternationalShippingRadioButton);
+            this.ShippingGroupBox.Controls.Add(this.ExpeditedShippingRadioButton);
+            this.ShippingGroupBox.Controls.Add(this.StandardShippingRadioButton);
+            this.ShippingGroupBox.Location = new System.Drawing.Point(12, 104);
+            this.ShippingGroupBox.Name = "ShippingGroupBox";
+            this.ShippingGroupBox.Size = new System.Drawing.Size(200, 107);
+            this.ShippingGroupBox.TabIndex = 3;
+            this.ShippingGroupBox.TabStop = false;
+            this.ShippingGroupBox.Text = "Shipping Options";
+            // 
+            // StandardShippingRadioButton
+            // 
+            this.StandardShippingRadioButton.AutoSize = true;
+            this.StandardShippingRadioButton.Checked = true;
+            this.StandardShippingRadioButton.Location = new System.Drawing.Point(6, 19);
+            this.StandardShippingRadioButton.Name = "StandardShippingRadioButton";
+            this.StandardShippingRadioButton.Size = new System.Drawing.Size(112, 17);
+            this.StandardShippingRadioButton.TabIndex = 4;
+            this.StandardShippingRadioButton.TabStop = true;
+            this.StandardShippingRadioButton.Text = "Standard Shipping";
+            this.StandardShippingRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ExpeditedShippingRadioButton
+            // 
+            this.ExpeditedShippingRadioButton.AutoSize = true;
+            this.ExpeditedShippingRadioButton.Location = new System.Drawing.Point(6, 42);
+            this.ExpeditedShippingRadioButton.Name = "ExpeditedShippingRadioButton";
+            this.ExpeditedShippingRadioButton.Size = new System.Drawing.Size(116, 17);
+            this.ExpeditedShippingRadioButton.TabIndex = 5;
+            this.ExpeditedShippingRadioButton.TabStop = true;
+            this.ExpeditedShippingRadioButton.Text = "Expedited Shipping";
+            this.ExpeditedShippingRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // InternationalShippingRadioButton
+            // 
+            this.InternationalShippingRadioButton.AutoSize = true;
+            this.InternationalShippingRadioButton.Location = new System.Drawing.Point(6, 65);
+            this.InternationalShippingRadioButton.Name = "InternationalShippingRadioButton";
+            this.InternationalShippingRadioButton.Size = new System.Drawing.Size(127, 17);
+            this.InternationalShippingRadioButton.TabIndex = 6;
+            this.InternationalShippingRadioButton.TabStop = true;
+            this.InternationalShippingRadioButton.Text = "International Shipping";
+            this.InternationalShippingRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ShippingChargesLabel
+            // 
+            this.ShippingChargesLabel.AutoSize = true;
+            this.ShippingChargesLabel.Location = new System.Drawing.Point(15, 292);
+            this.ShippingChargesLabel.Name = "ShippingChargesLabel";
+            this.ShippingChargesLabel.Size = new System.Drawing.Size(90, 13);
+            this.ShippingChargesLabel.TabIndex = 7;
+            this.ShippingChargesLabel.Text = "Shipping Charges";
+            // 
+            // ShippingChargesTextBox
+            // 
+            this.ShippingChargesTextBox.Enabled = false;
+            this.ShippingChargesTextBox.Location = new System.Drawing.Point(111, 285);
+            this.ShippingChargesTextBox.Name = "ShippingChargesTextBox";
+            this.ShippingChargesTextBox.ReadOnly = true;
+            this.ShippingChargesTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ShippingChargesTextBox.TabIndex = 8;
+            this.ShippingChargesTextBox.TabStop = false;
+            // 
+            // CalculateShippingChargesButton
+            // 
+            this.CalculateShippingChargesButton.Location = new System.Drawing.Point(18, 323);
+            this.CalculateShippingChargesButton.Name = "CalculateShippingChargesButton";
+            this.CalculateShippingChargesButton.Size = new System.Drawing.Size(75, 23);
+            this.CalculateShippingChargesButton.TabIndex = 8;
+            this.CalculateShippingChargesButton.Text = "Calculate";
+            this.CalculateShippingChargesButton.UseVisualStyleBackColor = true;
+            this.CalculateShippingChargesButton.Click += new System.EventHandler(this.CalculateShippingChargesButton_Click);
+            // 
+            // ShippingWeightTextBox
+            // 
+            this.ShippingWeightTextBox.AcceptsReturn = true;
+            this.ShippingWeightTextBox.Location = new System.Drawing.Point(111, 236);
+            this.ShippingWeightTextBox.Name = "ShippingWeightTextBox";
+            this.ShippingWeightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ShippingWeightTextBox.TabIndex = 7;
+            // 
+            // ShippingWeightLabel
+            // 
+            this.ShippingWeightLabel.AutoSize = true;
+            this.ShippingWeightLabel.Location = new System.Drawing.Point(13, 239);
+            this.ShippingWeightLabel.Name = "ShippingWeightLabel";
+            this.ShippingWeightLabel.Size = new System.Drawing.Size(85, 13);
+            this.ShippingWeightLabel.TabIndex = 11;
+            this.ShippingWeightLabel.Text = "Shipping Weight";
+            // 
+            // ResetFormButton
+            // 
+            this.ResetFormButton.Location = new System.Drawing.Point(18, 356);
+            this.ResetFormButton.Name = "ResetFormButton";
+            this.ResetFormButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetFormButton.TabIndex = 12;
+            this.ResetFormButton.Text = "Reset Form";
+            this.ResetFormButton.UseVisualStyleBackColor = true;
+            this.ResetFormButton.Click += new System.EventHandler(this.ResetFormButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(189, 356);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 13;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(259, 152);
+            this.ClientSize = new System.Drawing.Size(276, 391);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ResetFormButton);
+            this.Controls.Add(this.ShippingWeightLabel);
+            this.Controls.Add(this.ShippingWeightTextBox);
+            this.Controls.Add(this.CalculateShippingChargesButton);
+            this.Controls.Add(this.ShippingChargesTextBox);
+            this.Controls.Add(this.ShippingChargesLabel);
+            this.Controls.Add(this.ShippingGroupBox);
             this.Controls.Add(this.ResultsTextBox);
             this.Controls.Add(this.CalculateButton);
             this.Controls.Add(this.WidthTextBox);
@@ -99,6 +233,8 @@
             this.Controls.Add(this.LengthLabel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.ShippingGroupBox.ResumeLayout(false);
+            this.ShippingGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,6 +248,17 @@
         private System.Windows.Forms.TextBox WidthTextBox;
         private System.Windows.Forms.Button CalculateButton;
         private System.Windows.Forms.TextBox ResultsTextBox;
+        private System.Windows.Forms.GroupBox ShippingGroupBox;
+        private System.Windows.Forms.RadioButton InternationalShippingRadioButton;
+        private System.Windows.Forms.RadioButton ExpeditedShippingRadioButton;
+        private System.Windows.Forms.RadioButton StandardShippingRadioButton;
+        private System.Windows.Forms.Label ShippingChargesLabel;
+        private System.Windows.Forms.TextBox ShippingChargesTextBox;
+        private System.Windows.Forms.Button CalculateShippingChargesButton;
+        private System.Windows.Forms.TextBox ShippingWeightTextBox;
+        private System.Windows.Forms.Label ShippingWeightLabel;
+        private System.Windows.Forms.Button ResetFormButton;
+        private System.Windows.Forms.Button ExitButton;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -20,15 +20,74 @@ namespace WindowsFormsApp1
         private void CalculateButton_Click(object sender, EventArgs e)
         {
             // var length = Convert.ToDouble(this.LengthTextBox.Text);
-            Double.TryParse(this.LengthTextBox.Text, out double length);
+            double.TryParse(this.LengthTextBox.Text, out double length);
 
             //var width = Convert.ToDouble(this.WidthTextBox.Text);
-            Double.TryParse(this.WidthTextBox.Text, out double width);
+            double.TryParse(this.WidthTextBox.Text, out double width);
 
             var area = length * width;
 
             this.ResultsTextBox.Text = $"The Area is : {area:F2}";
+        }
 
+        private void CalculateShippingChargesButton_Click(object sender, EventArgs e)
+        {
+            var StandardShippingRate = 1.0m;
+            var ExpeditedShippingCost = 2.0m;
+            var InternationalShippingCost = 3.0m;
+
+            var optionCost = 0.0m;
+            if (this.StandardShippingRadioButton.Checked)
+            {
+                optionCost = StandardShippingRate;
+            }
+            else if (this.ExpeditedShippingRadioButton.Checked)
+            {
+                optionCost = ExpeditedShippingCost;
+            }
+            else
+            { 
+                optionCost = InternationalShippingCost;  
+            }
+
+            if (decimal.TryParse(this.ShippingWeightTextBox.Text, out decimal weight))
+            {
+                if (weight < 0.0m)
+                {
+                    MessageBox.Show("Invalid Weight Entered!" +
+                                    "\nMust be greater than zero.", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    this.ShippingWeightTextBox.Clear();
+                    this.ShippingWeightTextBox.Focus();
+                }
+                else
+                {
+                    var total = optionCost + (0.1m * weight);
+                    this.ShippingChargesTextBox.Text = $"{total:C}";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Data Entered!" +
+                                "\nNon-Numeric Input.", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                this.ShippingWeightTextBox.Clear();
+                this.ShippingWeightTextBox.Focus();
+            }
+        }
+
+        private void ResetFormButton_Click(object sender, EventArgs e)
+        {
+            this.ShippingWeightTextBox.Clear();
+            this.ShippingWeightTextBox.Focus();
+            this.ShippingChargesTextBox.Clear();
+            this.ResultsTextBox.Text = string.Empty;
+            this.StandardShippingRadioButton.Checked = true;
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
